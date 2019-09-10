d81013bf503a964dc3d758c4c06e1c6ba6a9b2e2
ben <benedikt.rudolf@gmx.net>
2019-09-10 10:05:16 +0200

    Add friend search and adding and removing friends and view friends profile

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   get 'search_stocks', to: 'stocks#search'
   get 'my_friends', to: "users#my_friends"
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
+  get 'search_friends', to: "users#search"
+  post 'add_friend', to: 'users#add_friend'
 
 end
